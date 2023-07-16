f50c992e36c34d9d2880664ad6dd9be6605d7396
Priyansh Zalavadiya <priyanshzalavadiya007@gmail.com>
2023-07-16 14:26:41 -0700

    Vectore store added

---
+++ vectorStore/getfacts.py
@@ -0,0 +1,36 @@
+import Embedding
+import os
+
+#Set your coherekey here
+os.environ['cohere_key'] = ''
+
+
+directory_path = "./demo"
+embedding_pickle_path = "./sentence_embedding_func.pkl"
+vector_path = "./chroma_db_sentence_embd"
+
+Embedding.save_embedding_into_pickle(embedding_pickle_path)
+
+Embedding.save_chroma_using_embedding(directory_path,
+                            embedding_pickle_path,
+                            vector_path)
+
+
+#Now lets ask query to saved embeddings
+query = "What happened in japan?"
+#Cohere API key
+api_key = os.environ['cohere_key']
+
+#Ans1 contains all the facts which are there for asked Query
+ans1 = Embedding.load_chroma_with_query_without_compressor(vector_path,
+                       embedding_pickle_path,
+                       query)
+
+print(ans1)
+
+#Ans2 contains all the facts with compressor and reranker which are there for asked Query
+ans2 = Embedding.load_chroma_with_query_with_compressor(vector_path,
+                       embedding_pickle_path,
+                       query, api_key)
+
+print(ans2)
